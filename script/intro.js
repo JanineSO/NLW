552c1d3fa3fa848171149d92e853787a1e06b57a
JanineSO <janine.oliveira@hotmail.com>
2024-03-23 15:41:11 -0300

    Atualizando Site

---
+++ script/intro.js
@@ -0,0 +1,100 @@
+// coments 
+// document.write("Hello")
+
+//variaveis, tipos de dados
+// var myvar //undefined
+// myvar = "Hello"
+
+// document.write(myvar)
+
+// continuar introdução js...
+
+//Comentários
+// document.write("Hello")
+
+//Variáveis, tipo de dados
+//let = var
+//var myvar //undefined
+//let myvar
+//my myconst = "He"
+//myvar = "Hello"
+
+//document.write(myvar)
+//document.write(myconst)
+//string
+//const s1 = "isso é uma string"
+//const s2 = 'isso também é uma string'
+//const s3 = `isso é uma string também`
+
+//document.write(s2)
+
+//number
+//const n1 = 1
+//const n2 = 12
+
+//document.write(n1 + n2)
+
+//bolean - true ou false
+
+//const btrue = true
+//const bfalse = false
+//document.write(bfalse)
+
+//objeto
+// objeto possui propriedades e funcionalidades
+//const pessoa ={
+  //altura: "1,80m",
+  //idade: 24,
+  //solteiro: true,
+  //correr(){
+    //document.write("run Forest")
+  //  return "run Forest"
+      // document.write("executando uma grande lógica de correr")
+  //}
+//}
+
+//document.write(pessoa.altura)
+//document.write(pessoa.idade)
+//document.write(pessoa.solteiro)
+//document.write(pessoa.correr())
+
+//pessoa.correr()
+
+//arraay - vetores
+
+//const colecaoDeBolinhas = ["blue","green",1, {name:"My Name"}]
+
+//document.write(colecaoDeBolinhas[2])
+//document.write(colecaoDeBolinhas)
+//document.write(colecaoDeBolinhas[3].name)
+
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+//registrar função
+//function sayMyName(name){
+//  document.write(name)
+//}
+
+//executar
+//sayMyName(Douglas)
+//sayMyName(Valeska)
+//sayMyName(Kyan)
+
+//Condicional
+
+//const notaFinal = 4
+
+//if( notaFinal < 5 ){
+  //document.write("Reprovado")
+//} else {
+//  document.write("Aprovado") 
+//}
+
+//loop repetições
+
+//for (i=0; i < 10; i++){
+  //document.write("<p> aló </p>")
+  //document.write(`<p>${i}</p>`)
+  
+//}
